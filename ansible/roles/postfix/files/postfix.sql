7fdb1d2117d243b3e1a18519c5815d42cd8b21bf
Reed Kraft-Murphy <reed@reedmurphy.net>
2014-01-24 17:07:44 +1100

    Reduce length of `email' so it can be indexed

--- ansible/roles/postfix/files/postfix.sql
+++ ansible/roles/postfix/files/postfix.sql
@@ -7,7 +7,7 @@ CREATE TABLE `virtual_users` (
   `id` int(11) NOT NULL auto_increment,
   `domain_id` int(11) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(355) NOT NULL,
+  `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
